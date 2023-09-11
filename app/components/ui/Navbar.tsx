e1b2c02b89aac6373a86630501cc4a533be50f55
Ali <ali_mohamadi2017@yahoo.com>
2023-09-11 09:30:18 +0330

    Initial Project

---
+++ app/components/ui/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center pt-3 px-5 justify-between xl:px-20 md:px-10 sm:px-8">
+      <div>Icon</div>
+      <div>BlogAli</div>
+      <div>Links</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" min-h-screen xl:ml-20 xl:mr-20 md:ml-10 md:mr-10 sm:m-0 p-5 ">
+      <h1>hello world!</h1>{" "}
+    </main>
+  );
+}
