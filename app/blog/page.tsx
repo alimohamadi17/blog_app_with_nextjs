bafe6ac68bd548a6690287be1dbd619ef5ea98dd
Ali <ali_mohamadi2017@yahoo.com>
2023-09-12 18:44:14 +0330

    Edit style Blog Page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,13 +4,13 @@ import Menu from "../components/Menu";
 
 const BlogPage = () => {
   return (
-    <div className="px-20">
-      <div className=" my-16 bg-red-500 p-3">
+    <div className="px-20 ">
+      <div className=" my-16 bg-red-500 p-4 rounded-t-full">
         <h1 className="text-center text-2xl font-semibold text-white">
           Style Blog
         </h1>
       </div>
-      <div className="flex ">
+      <div className="flex gap-x-2">
         <CartList />
         <Menu />
       </div>
